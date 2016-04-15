2bbd93b8725d0247c4f91f1f7209247e5f718081
Taylor Traverse <taylorvt802@gmail.com>
2016-04-15 13:11:27 -0400

    updated android root file

--- index.android.js
+++ index.android.js
@@ -11,19 +11,13 @@ import React, {
   View
 } from 'react-native';
 
+import App from  './src/components/App';
+
 class reactnativestarter extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+        <App />
       </View>
     );
   }
